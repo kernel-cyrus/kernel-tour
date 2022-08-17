9e0876155031deb0cb473b68a04bb6a8258b5bad
Cyrus Huang <cyrus.kernel@gmail.com>
2022-08-17 18:31:13 +0800

    Finish sysfs and test-sysfs

--- docs/fs/sysfs.md
+++ docs/fs/sysfs.md
@@ -2,7 +2,41 @@
 
 > sysfs is a filesystem for exporting kernel objects(kobject)
 
+sysfs是系统中所有kobject的layout。每个kobject，对应sysfs中的一个目录。kobject可以创建自己的attribute，每个attribute在sysfs中对应一个文件。
 
+每个kobject在kobject->ktype->sysfs_ops中，定义show、store函数，kobject下的所有attribute文件，都共用这个show、store，来响应cat、echo操作。
+
+一般情况下，各个模块会定义自己私有数据结构，在私有数据结构中定义出自己私有数据结构的show、store，这个二级show、store，通过sysfs_ops中的show、store转发过来。（二级数据结构封装）
+
+各个子系统的实现中，都用这种二级数据结构封装的方式实现了自己的data、attr结构体，相关定义、初始化宏，以及操作函数。比如device、bus、driver，这些kobject都有自己的二级封装。
 
 # Files
 
+```
+- /include/linux/sysfs.h	# sysfs interface
+- /fs/sysfs/*			# sysfs implementation
+```
+
+# Interface
+
+`sysfs_create_file`
+
+创建一个attr节点。
+
+`sysfs_create_group`
+
+创建一批attr节点（attr group，一个attr数组）
+
+`__ATTR_XXX`
+
+简易的二级数据结构赋值宏
+
+# Module Test
+
+kobject and attr group:
+
+[test-kobject](https://github.com/kernel-cyrus/kernel-tour/tree/master/tests/test-kobject)
+
+my_data / my_attr implementation:
+
+[test-sysfs](https://github.com/kernel-cyrus/kernel-tour/tree/master/tests/test-sysfs)
--- tests/test-sysfs/sysfs.c
+++ tests/test-sysfs/sysfs.c
@@ -1,22 +1,75 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sysfs.h>
 
 #include "../include/test-modules.h"
 
-static int test_hello_init(void)
+struct my_data_type {
+	struct kobject kobj;
+	char data[32];
+};
+
+struct my_attr_type {
+	struct attribute attr;
+	ssize_t (*show)(struct my_data_type *my_data, struct my_attr_type *my_attr,
+		    char *buf);
+	ssize_t (*store)(struct my_data_type *my_data, struct my_attr_type *my_attr,
+		    const char *buf, size_t count);
+};
+
+/* Create kobject with show */
+
+static struct my_data_type my_data = {
+	.data = "This is my data."
+};
+
+static ssize_t test_show(struct kobject *kobj, struct attribute *attr, char *buf)
 {
-	pr_tour("Module init: test-hello");
+	ssize_t ret = -EIO;
+	struct my_data_type* my_data = container_of(kobj, struct my_data_type, kobj);
+	struct my_attr_type* my_attr = container_of(attr, struct my_attr_type, attr);
+	ret = sysfs_emit(buf, "hello sysfs.\n");
+	if (my_attr->show)
+		ret += my_attr->show(my_data, my_attr, buf);
+	return ret;
+}
+
+static struct sysfs_ops test_ops = {
+	.show = test_show,
+	.store = NULL
+};
+
+static struct kobj_type my_ktype = {
+	.sysfs_ops = &test_ops
+};
+
+/* Create attrs, attr show should be dispatched from kobject show */
+
+static ssize_t test_attr_show(struct my_data_type *my_data, struct my_attr_type *my_attr, char *buf)
+{
+	return sysfs_emit(buf, "%s\n", my_data->data);
+}
+
+static struct my_attr_type test_attr = __ATTR_RO(test_attr);
+
+static int test_sysfs_init(void)
+{
+	pr_tour("Module init: test-sysfs");
+	// Create my_policy directory, add show function for all attrs.
+	kobject_init_and_add(&my_data.kobj, &my_ktype, kernel_kobj, "my_policy");
+	// Add attr and dispatch show to attr_show
+	sysfs_create_file(&my_data.kobj, &test_attr.attr);
 	return 0;
 }
 
-static void test_hello_exit(void)
+static void test_sysfs_exit(void)
 {
-	pr_tour("Module exit: test-hello");
+	pr_tour("Module exit: test-sysfs");
 	return;
 }
 
-late_initcall(test_hello_init);
-module_exit(test_hello_exit);
+late_initcall(test_sysfs_init);
+module_exit(test_sysfs_exit);
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Cyrus Huang");
